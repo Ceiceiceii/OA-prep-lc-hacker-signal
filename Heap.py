3ea7cc836785ab6e64abfe097ba69fd9ae3bef4a
Tracy Zhang <91210454+Ceiceiceii@users.noreply.github.com>
2023-11-18 14:08:04 -0500

    Update Heap.py

--- Heap.py
+++ Heap.py
@@ -1,1 +1,14 @@
+# Heapq  Module:
+import heapq
 
+# min-heap: heap[0] -> the smallest
+# the heap will return or pop the smallest or largest while make the heap structure the same
+
+# LC: 215. Kth Largest Element in an Array
+def findKthLargest(self, nums, k):
+  heap = []
+  for num in nums:
+    heapq.heappush(heap, num)
+    if len(heap)>k:
+      heapq.heappop(heap)
+  return heap[0]
