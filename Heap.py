2596c92d66870f9713fa0144e70372c36da34306
Tracy Zhang <91210454+Ceiceiceii@users.noreply.github.com>
2023-11-18 18:24:02 -0500

    Update Heap.py

--- Heap.py
+++ Heap.py
@@ -12,3 +12,32 @@ def findKthLargest(self, nums, k):
     if len(heap)>k:
       heapq.heappop(heap)
   return heap[0]
+
+
+#LC: 2336. Smallest Number in Infinite Set
+class SmallestInfiniteSet(object):
+
+    def __init__(self):
+        self.current = 1 #heap[0] initially is 1
+        self.heap = []
+
+    def popSmallest(self):
+        """
+        :rtype: int
+        """
+        if len(self.heap)!=0:
+            return heapq.heappop(self.heap)
+        else:
+            self.current +=1
+            return self.current-1
+
+    def addBack(self, num):
+        """
+        :type num: int
+        :rtype: None
+        """
+        if num < self.current-1 and num not in self.heap:
+            heapq.heappush(self.heap,num)
+        elif num == self.current-1:
+            self.current = num
+
